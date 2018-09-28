1aa6763b6e51a33bb0e908c48ace1ed41a4137d4
Sean Spratt <seansp@gmail.com>
2018-09-28 10:29:31 -0700

    Changed username in azurestorage from seansp to mule.

--- copy_17744_from_azure_to_public.ps1
+++ copy_17744_from_azure_to_public.ps1
@@ -1,3 +1,3 @@
-$mine = New-AzureStorageContext -StorageAccountName seansp -StorageAccountKey "$args"
+$mine = New-AzureStorageContext -StorageAccountName mule -StorageAccountKey "$args"
 $blob = Get-AzureStorageBlob -Context $mine -Container vhdx | Where-Object {$_.Name -eq "Windows_InsiderPreview_ServerStandard_en-us_VHDX_17744.vhdx"}
 Get-AzureStorageBlobContent -Blob $blob.Name -Context $mine -Container vhdx -Destination C:\Users\Public\Windows_InsiderPreview_ServerStandard_en-us_VHDX_17744.vhdx
