c9ce07a8ce95dfdbdeb41e5c5ba89f73828d9315
Sean Spratt <seansp@gmail.com>
2018-10-03 16:01:18 -0700

    creds.

--- initTBD2.ps1
+++ initTBD2.ps1
@@ -7,8 +7,7 @@ $ips = Get-VM -VMName $args | Select -ExpandProperty NetworkAdapters | Select IP
 $ipv4 = $ips.IPAddresses[0]
 $trusthosts = (Get-Item -Path WSMan:\localhost\Client\TrustedHosts).Value
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts "$trusthosts, $ipv4" -Force
-Invoke-Command -ComputerName $ipv4 -ScriptBlock {HostName}
-Invoke-Command -ComputerName $ipv4 -ScriptBlock {param($p1) Rename-Computer -NewName "$p1" -Restart -Force} -ArgumentList "$args"
-Sleep 30
-#Get-VM | ?{$_.Name -eq “$args”} | Select -ExpandProperty NetworkAdapters | Select IPAddresses
-Invoke-Command -ComputerName $ipv4 -ScriptBlock {HostName}
+
+$password = "p@ssw0rd1234" | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList "mstest", $password
+Invoke-Command -ComputerName $ipv4 -ScriptBlock {param($p1) Rename-Computer -NewName "$p1" -Restart -Force} -ArgumentList "$args" -Credential $cred
