6139eba13f37551e0d3f6e3029bac738385350ac
Sean Spratt <seansp@gmail.com>
2018-10-04 15:07:59 -0700

    try catch

--- initTBD4.ps1
+++ initTBD4.ps1
@@ -7,9 +7,13 @@ $ips = Get-VM -VMName $args | Select -ExpandProperty NetworkAdapters | Select IP
 $ipv4 = $ips.IPAddresses[0]
 $trusthosts = (Get-Item -Path WSMan:\localhost\Client\TrustedHosts).Value
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts "$trusthosts, $ipv4" -Force
-
-$password = ConvertTo-SecureString -String "p@ssw0rd1234" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList "mstest", $password
-$payload = "Invoke-Command -ComputerName $ipv4 -ScriptBlock { Rename-Computer -NewName $args -Restart -Force }"
-Set-Content -Path c:\users\public\payload.ps1 -Value $payload
-Start-Process powershell.exe -ArgumentList c:\users\public\payload.ps1 -NoNewWindow -Credential $cred -RedirectStandardError c:\users\public\payload.stderr.log -RedirectStandardOutput c:\users\public\payload.stdout.log
+try {
+    $password = ConvertTo-SecureString -String "p@ssw0rd1234" -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList "mstest", $password
+    $payload = "Invoke-Command -ComputerName $ipv4 -ScriptBlock { Rename-Computer -NewName $args -Restart -Force }"
+    Set-Content -Path c:\users\public\payload.ps1 -Value $payload
+    Start-Process powershell.exe -ArgumentList c:\users\public\payload.ps1 -NoNewWindow -Credential $cred -RedirectStandardError c:\users\public\payload.stderr.log -RedirectStandardOutput c:\users\public\payload.stdout.log    
+}
+catch {
+    $_
+}
