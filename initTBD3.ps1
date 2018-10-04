cd3e8e076bc4e28fb37b1919cc15fcbc999cb870
Sean Spratt <seansp@gmail.com>
2018-10-04 14:59:13 -0700

    adding redirects for stderr/output

--- initTBD3.ps1
+++ initTBD3.ps1
@@ -12,4 +12,4 @@ $password = ConvertTo-SecureString -String "p@ssw0rd1234" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList "mstest", $password
 $payload = "Invoke-Command -ComputerName $ipv4 -ScriptBlock { Rename-Computer -NewName $args -Restart -Force }"
 Set-Content -Path c:\users\public\payload.ps1 -Value $payload
-Start-Process powershell.exe -ArgumentList c:\users\public\payload.ps1 -NoNewWindow -Credential $cred
+Start-Process powershell.exe -ArgumentList c:\users\public\payload.ps1 -NoNewWindow -Credential $cred -RedirectStandardError c:\users\public\payload.stderr.log -RedirectStandardOutput c:\users\public\payload.stdout.log
