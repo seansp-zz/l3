6eddcf8067b214f5cb972507fa7dc0a9e479d573
Sean Spratt <seansp@gmail.com>
2018-10-10 08:54:55 -0700

    Added Build-HGS to tools.

--- tools/Get-Tools.ps1
+++ tools/Get-Tools.ps1
@@ -1,12 +1,13 @@
 Param( [Parameter(Mandatory=$true)][string] $userName, 
        [Parameter(Mandatory=$true)][string] $password )
-       
+
 Write-Host "Retrieving Tools from l3"
 $toolRoot = "https://raw.githubusercontent.com/seansp/l3/master/tools"
 $tools = @()
 $tools += "Get-TrustedHosts.ps1"
 $tools += "Get-VMs.ps1"
 $tools += "Get-HostnameFromVM.ps1"
+$tools += "Build-HGS.ps1"
 foreach( $tool in $tools )
 {
     Write-Host "Retrieving $tool to local folder."
