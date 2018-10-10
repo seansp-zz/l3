03a33c7bf4f02ea9a170a6ce4fe432d9ebc0bbfa
Sean Spratt <seansp@gmail.com>
2018-10-10 09:32:06 -0700

    Added script to start new scheduled task.

---
+++ tools/Schedule-Build-HGS.ps1
@@ -0,0 +1,21 @@
+Param(
+    [Parameter(Mandatory=$true)][string] $VMName,
+    [Parameter(Mandatory=$true)][string] $adminUsername,
+    [Parameter(Mandatory=$true)][string] $adminPassword,
+    [Parameter(Mandatory=$true)][System.Int64] $memorySize )
+  
+  $now = [System.DateTime]::Now
+  $date = ""
+  if( $now.Month -lt 10 ) { $date = "0" }
+  $date = "$date$($now.Month)"
+  if( $now.Day -lt 10 ) { $date = "$date/0$($now.Day)" }
+  else { $date = "$date/$($now.Day)" }
+  $date = "$date/$($now.Year)"
+  $now = $now.AddMinutes(1)
+  $time = "$($now.Hour):"
+  if( $now.Hour -lt 10 ) { $time = "0$time" }
+  if( $now.Minute -lt 10) { $time = "$($time)0$($now.Minute)" }
+  else { $time = "$time$($now.Minute)" }
+  
+  $taskPath = "C:\users\Public\Build-HGS.ps1"
+  & schtasks.exe /CREATE /F /RL HIGHEST /RU $adminUsername /RP $adminPassword /SC ONCE /S LocalHost /TR "powershell.exe -ExecutionPolicy ByPass -File $taskPath -VMName $VMName -adminUsername $adminUsername -adminPassword $adminPassword -memorySize $memorySize" /TN "Build HGS on $VMName" /SD $date /ST $time
