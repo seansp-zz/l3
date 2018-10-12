1cb1ed5bd5d594da4214d40c6955b01928a13438
Sean Spratt <seansp@gmail.com>
2018-10-12 11:43:17 -0700

    Trying explicit -Outfile instead of my write.

--- tools/Get-Tools.ps1
+++ tools/Get-Tools.ps1
@@ -1,13 +1,11 @@
 Write-Host "Retrieving Tools from l3"
 $toolRoot = "https://raw.githubusercontent.com/seansp/l3/master/tools"
+
 $tools = @()
 $tools += "Build-HGS.ps1"
 $tools += "Build-GuardedHost.ps1"
 $tools += "Schedule-Build-HGS.ps1"
 foreach( $tool in $tools )
 {
-    Write-Host "Retrieving $tool to local folder."
-    $src = wget $toolRoot/$tool
-    Set-Content -Path C:\Users\Public\$tool -Value $src
+    wget $toolRoot/$tool -Outfile c:\users\public\$tool
 }
-Write-Host "Finished."
