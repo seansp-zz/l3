ea2275c86557c416c026898ac7dbd22079ab3d0c
Sean Spratt <seansp@gmail.com>
2018-10-12 11:24:26 -0700

    Simplified tool retrieval. Always empty, why?

--- tools/Get-Tools.ps1
+++ tools/Get-Tools.ps1
@@ -1,12 +1,6 @@
-Param( [Parameter(Mandatory=$true)][string] $userName, 
-       [Parameter(Mandatory=$true)][string] $password )
-
 Write-Host "Retrieving Tools from l3"
 $toolRoot = "https://raw.githubusercontent.com/seansp/l3/master/tools"
 $tools = @()
-$tools += "Get-TrustedHosts.ps1"
-$tools += "Get-VMs.ps1"
-$tools += "Get-HostnameFromVM.ps1"
 $tools += "Build-HGS.ps1"
 $tools += "Build-GuardedHost.ps1"
 $tools += "Schedule-Build-HGS.ps1"
@@ -14,8 +8,6 @@ foreach( $tool in $tools )
 {
     Write-Host "Retrieving $tool to local folder."
     $src = wget $toolRoot/$tool
-    $src = $src -replace "AUTOMATION_USERNAME", $userName
-    $src = $src -replace "AUTOMATION_PASSWORD", $password
     Set-Content -Path C:\Users\Public\$tool -Value $src
 }
 Write-Host "Finished."
