6119fe9a99bb1b5c4e5015ba766bc468fea11a18
Sean Spratt <seansp@gmail.com>
2018-12-01 13:33:53 -0800

    fixing prior bad file.

--- azure/hgs/ConfigureHGS-Step4.ps1
+++ azure/hgs/ConfigureHGS-Step4.ps1
@@ -1,5 +1,10 @@
-Disable-HgsAttestationPolicy Hgs_IommuEnabled
-$name = 'Guarded Hosts'
-New-ADGroup -Name $name -GroupScope Global -GroupCategory Security
-$group = Get-ADGroup $name
-Add-HgsAttestationHostGroup -Name $group.Name -Identifier $group.SID
+$password = ConvertTo-SecureString -String "$($args[0])" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $($args[1]), $password
+
+$script = {
+    $name = 'Guarded Hosts'
+    New-ADGroup -Name $name -GroupScope Global -GroupCategory Security
+    $group = Get-ADGroup $name
+    Add-HgsAttestationHostGroup -Name $group.Name -Identifier $group.SID
+}
+Invoke-Command -ComputerName localhost -Credential $cred -ScriptBlock $script
