4ed1e40ea9c5c295efdadc0ea9fa0fc42f458dde
Sean Spratt <seansp@gmail.com>
2018-12-01 03:54:13 -0800

    attestation.2

--- azure/guardedhost/ConfigureAttestation.ps1
+++ azure/guardedhost/ConfigureAttestation.ps1
@@ -1,4 +1,10 @@
 $password = ConvertTo-SecureString -String "$($args[0])" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $($args[1]), $password
-Set-HgsClientConfiguration -AttestationServerUrl "http://hgs.$($args[2]).com/Attestation" -KeyProtectionServerUrl "http://hgs.$($args[2]).com/KeyProtection" -Credential $cred
+
+$ghostHgsClientConfiguration = { 
+    Set-HgsClientConfiguration -AttestationServerUrl "http://hgs.$args.com/Attestation" -KeyProtectionServerUrl "http://hgs.$args.com/KeyProtection"
+}
+Invoke-Command -Credential $cred -ScriptBlock $ghostHgsClientConfiguration -ArgumentList $args[2]
+
+
 #Invoke-Command -ComputerName $ghostip -Credential $cred -ScriptBlock $ghostJoinDomain -ArgumentList $adminPassword, "$Domain\Administrator", $Domain
