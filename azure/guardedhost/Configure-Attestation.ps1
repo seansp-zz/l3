f694947203548256458790ed48b8931e4bd8e071
Sean Spratt <seansp@gmail.com>
2018-12-01 20:35:14 -0800

    logging

--- azure/guardedhost/Configure-Attestation.ps1
+++ azure/guardedhost/Configure-Attestation.ps1
@@ -1,10 +1,20 @@
+Import-Module C:\users\public\Microsoft.LSG.Utilities.psm1 -Force
+Start-LSGNotes -path c:\users\public\Configure-Attestation.1.log
+
+Write-LSGNote "building password"
 $password = ConvertTo-SecureString -String "$($args[0])" -AsPlainText -Force
+Write-LSGNote "building credential"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $($args[1]), $password
 
-$ghostHgsClientConfiguration = { 
+$ghostHgsClientConfiguration = {
+    Import-Module C:\users\public\Microsoft.LSG.Utilities.psm1 -Force
+    Start-LSGNotes -path c:\users\public\Configure-Attestation.1.ghost.log
+    Write-LSGNote "Using AttestationServerUrl - http://hgs.$args.com/Attestation"
+    Write-LSGNote "Using KeyProtectuinServerUrl - http://hgs.$args.com/KeyProtection"
     Set-HgsClientConfiguration -AttestationServerUrl "http://hgs.$args.com/Attestation" -KeyProtectionServerUrl "http://hgs.$args.com/KeyProtection"
+    Write-LSGNote "Finished."
 }
+Write-LSGNote "Invoking localhost..."
 Invoke-Command -ComputerName localhost -Credential $cred -ScriptBlock $ghostHgsClientConfiguration -ArgumentList $($args[2])
-
-
+Write-LSGNote "Finished."
 #Invoke-Command -ComputerName $ghostip -Credential $cred -ScriptBlock $ghostJoinDomain -ArgumentList $adminPassword, "$Domain\Administrator", $Domain
--- azure/hgs/AddGroupMember.ps1
+++ azure/hgs/AddGroupMember.ps1
@@ -1,4 +1,11 @@
+Import-Module C:\users\public\Microsoft.LSG.Utilities.psm1 -Force
+Start-LSGNotes -path c:\users\public\AddGroupMember.log
+
+Write-LSGNote "building password"
 $password = ConvertTo-SecureString -String "$($args[0])" -AsPlainText -Force
+Write-LSGNote "building credential"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $($args[1]), $password
+Write-LSGNote "Adding GuardedHost to Guarded Hosts."
 Add-ADGroupMember "Guarded Hosts" -Members GuardedHost$ -Credential $cred
+Write-LSGNote "Finished."
 #Invoke-Command -ComputerName $ghostip -Credential $cred -ScriptBlock $ghostJoinDomain -ArgumentList $adminPassword, "$Domain\Administrator", $Domain
