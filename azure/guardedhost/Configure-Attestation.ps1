85978ce9747af174b26e3cc4caffb4bfc0878b95
Sean Spratt <seansp@gmail.com>
2018-12-01 20:51:58 -0800

    error reporting in log.

--- azure/guardedhost/Configure-Attestation.ps1
+++ azure/guardedhost/Configure-Attestation.ps1
@@ -1,5 +1,5 @@
 Import-Module C:\users\public\Microsoft.LSG.Utilities.psm1 -Force
-Start-LSGNotes -path c:\users\public\Configure-Attestation.1.log
+Start-LSGNotes -path c:\users\public\Configure-Attestation.log
 
 Write-LSGNote "building password"
 $password = ConvertTo-SecureString -String "$($args[0])" -AsPlainText -Force
@@ -12,9 +12,19 @@ $ghostHgsClientConfiguration = {
     Write-LSGNote "Using AttestationServerUrl - http://hgs.$args.com/Attestation"
     Write-LSGNote "Using KeyProtectuinServerUrl - http://hgs.$args.com/KeyProtection"
     Set-HgsClientConfiguration -AttestationServerUrl "http://hgs.$args.com/Attestation" -KeyProtectionServerUrl "http://hgs.$args.com/KeyProtection"
+    if( -not $? )
+    {
+        $issue = $Error[0].Exception.Message
+        Write-LSGNote "ERROR::$issue"
+    }
     Write-LSGNote "Finished."
 }
 Write-LSGNote "Invoking localhost..."
 Invoke-Command -ComputerName localhost -Credential $cred -ScriptBlock $ghostHgsClientConfiguration -ArgumentList $($args[2])
+if( -not $? )
+{
+    $issue = $Error[0].Exception.Message
+    Write-LSGNote "ERROR::$issue"
+}
 Write-LSGNote "Finished."
 #Invoke-Command -ComputerName $ghostip -Credential $cred -ScriptBlock $ghostJoinDomain -ArgumentList $adminPassword, "$Domain\Administrator", $Domain
--- azure/hgs/AddGroupMember.ps1
+++ azure/hgs/AddGroupMember.ps1
@@ -7,5 +7,10 @@ Write-LSGNote "building credential"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $($args[1]), $password
 Write-LSGNote "Adding GuardedHost to Guarded Hosts."
 Add-ADGroupMember "Guarded Hosts" -Members GuardedHost$ -Credential $cred
+if( -not $? )
+{
+    $issue = $Error[0].Exception.Message
+    Write-LSGNote "ERROR::$issue"
+}
 Write-LSGNote "Finished."
 #Invoke-Command -ComputerName $ghostip -Credential $cred -ScriptBlock $ghostJoinDomain -ArgumentList $adminPassword, "$Domain\Administrator", $Domain
